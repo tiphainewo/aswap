b70737bdfee17c8f2ca8f3a4e242cf533bb98c1d
tiphainewo <tiphainewo@gmail.com>
2022-10-25 15:51:01 +0200

    chat mockup

--- src/components/BottomNav.vue
+++ src/components/BottomNav.vue
@@ -1,22 +1,22 @@
 <template>
-  <v-bottom-navigation class="sticky bottom-0" fixed>
+  <v-bottom-navigation class="sticky bottom-0 w-full" fixed>
     <v-btn v-on:click="goTo('home')"
-      ><span>Catalogue</span>
+      ><span v-if="currentPage === 'home'">Catalogue</span>
       <v-icon>mdi-home</v-icon>
     </v-btn>
 
     <v-btn v-on:click="goTo('map')"
-      ><span>Carte</span>
-      <v-icon>mdi-home</v-icon>
+      ><span v-if="currentPage === 'map'">Carte</span>
+      <v-icon>mdi-map</v-icon>
     </v-btn>
 
     <v-btn v-on:click="goTo('chat')"
-      ><span>Discussions</span>
+      ><span v-if="currentPage === 'chat'">Discussions</span>
       <v-icon>mdi-forum</v-icon>
     </v-btn>
 
     <v-btn v-on:click="goTo('profile')"
-      ><span>Profil</span>
+      ><span v-if="currentPage === 'profile'">Profil</span>
       <v-icon>mdi-account</v-icon>
     </v-btn>
   </v-bottom-navigation>
@@ -26,9 +26,19 @@
 export default {
   name: "BottomNav",
 
+  data() {
+    return {};
+  },
+
   methods: {
     goTo(page) {
       this.$router.push(`/${page}`);
+    },
+  },
+
+  computed: {
+    currentPage() {
+      return this.$route.name;
     },
   },
 };
