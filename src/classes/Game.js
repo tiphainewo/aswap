fb044747782bd8a6032ec08bcee7069d514b0cd8
tiphainewo <tiphainewo@gmail.com>
2022-10-25 11:14:06 +0200

    update

--- src/classes/Game.js
+++ src/classes/Game.js
@@ -3,15 +3,17 @@ export default class Game {
     id = null;
     name = null;
     image = null;
+    price = null;
     userId = null;
     user = null;
  
-    constructor(id, name, image, userid) 
+    constructor(id, name, image, price, userid) 
     {
       this.id = id;
       this.name = name;
       this.image = image;
       this.userId = userid;
+      this.price = price;
     }
          
 }
