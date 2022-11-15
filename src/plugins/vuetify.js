91bdcf0f75e6bb4e50a03a5f8ff7e0db4843f3dc
tiphainewo <tiphainewo@gmail.com>
2022-11-15 14:32:22 +0100

    header start

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,4 +4,13 @@ import Vuetify from 'vuetify/lib/framework';
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    theme: {
+        themes: {
+          light: {
+            primary: '#D4D352',
+            secondary: '#D94693',
+            accent: '#50398E',
+          },
+        },
+      },
 });
