eacac671e1ab81b269564fa709c350cd9fe9e06c
Tiphaine <tiphainewo@gmail.com>
2022-11-24 10:30:33 +0100

    good progress on maps

--- src/components/pages/MapPage.vue
+++ src/components/pages/MapPage.vue
@@ -1,48 +1,61 @@
 <template>
-  <div id="mapContainer" class="basemap"></div>
+
+  <MglMap
+    :accessToken="accessToken"
+    :mapStyle.sync="mapStyle"
+    @load="onMapLoaded"
+    :center="center"
+    :zoom="zoom"
+  >
+  <MglNavigationControl position="top-right"/>
+    <MglGeolocateControl position="top-right" />
+    <MglMarker :coordinates="coordinates" color="blue" >
+      <!-- <v-icon slot="marker">mdi-map-marker</v-icon> -->
+      <MglPopup>
+        <VCard>
+          <div>Hello, I'm popup!</div>
+        </VCard>
+      </MglPopup>
+    </MglMarker>
+    </MglMap>
+
 </template>
 
 <script>
-import mapboxgl from "mapbox-gl";
+import Mapbox from "mapbox-gl";
+import { MglMap, MglNavigationControl, MglGeolocateControl, MglMarker, MglPopup } from "vue-mapbox";
 
 export default {
-  name: "BaseMap",
+  components: {
+    MglMap,
+    MglNavigationControl,
+    MglGeolocateControl,
+    MglMarker,
+    MglPopup
+  },
   data() {
     return {
-      accessToken:
-        "pk.eyJ1IjoicHJvamV0aW50ZWdyZTIwMjEiLCJhIjoiY2t1NWczYmoyMDdnYjJxcGFycnEwYTZpbCJ9.l5DP13cyiFb7yyokZhg1Cg",
+      accessToken: "pk.eyJ1IjoicHJvamV0aW50ZWdyZTIwMjEiLCJhIjoiY2t1NWczYmoyMDdnYjJxcGFycnEwYTZpbCJ9.l5DP13cyiFb7yyokZhg1Cg",
+      mapStyle: "mapbox://styles/mapbox/streets-v10",
+      coordinates: [4.829539, 45.740351],
+      center: [4.835659, 45.748043],
+      zoom: 13,
     };
   },
-  mounted() {
-    mapboxgl.accessToken = this.accessToken;
 
-    let map = new mapboxgl.Map({
-      container: "mapContainer",
-      style: "mapbox://styles/mapbox/streets-v10",
-      center: [4.835659, 45.764043],
-      zoom: 15,
-    });
-
-    const nav = new mapboxgl.NavigationControl();
-    map.addControl(nav, "top-right");
-
-    new mapboxgl.Marker().setLngLat([4.872709, 45.740853]).addTo(map);
-
-    const geolocate = new mapboxgl.GeolocateControl({
-      positionOptions: {
-        enableHighAccuracy: true,
-      },
-      trackUserLocation: true,
-    });
-
-    map.addControl(geolocate, "top-right");
+  created() {
+    // We need to set mapbox-gl library here in order to use it in template
+    this.mapbox = Mapbox;
   },
+
+  methods: {
+    onMapLoaded(event) {
+      // in component
+      this.map = event.map;
+      // or just to store if you want have access from other components
+      this.$store.map = event.map;
+    }
+  }
 };
 </script>
 
-<style lang="scss" scoped>
-.basemap {
-  width: 100%;
-  height: 100%;
-}
-</style>
