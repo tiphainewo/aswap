0643266ec6a7b301e68f8788773e2e827cdab3e9
Tiphaine <tiphainewo@gmail.com>
2022-11-29 09:32:59 +0100

    started page with user details

--- src/components/pages/MapPage.vue
+++ src/components/pages/MapPage.vue
@@ -36,7 +36,7 @@ export default {
     MglMarker
   },
 
-  props: ["users", "test"],
+  props: ["users"],
 
 
   data() {
--- src/components/pages/UserDetailsPage.vue
+++ src/components/pages/UserDetailsPage.vue
@@ -1,7 +1,7 @@
 <template>
-    <v-main class="d-flex justify-center align-center">
-        <div v-if="user">
-            <v-avatar size="50" bordered color="white" class="border-2" v-if="user.userImage">
+    <v-main class="flex flex-col items-center mt-5 mx-5 gap-10" v-if="user">
+        <div class="flex flex-col items-center">
+            <v-avatar size="80" bordered color="white" class="border-2" v-if="user.userImage">
                 <v-img :src="user.userImage"></v-img>
             </v-avatar>
 
@@ -10,19 +10,57 @@
                     mdi-account-circle
                 </v-icon>
             </v-avatar>
-
-
+            <p>{{ user.firstName + ' ' + user.lastName }}</p>
+            <v-rating length="5" :value="4.5" dense half-increments></v-rating>
         </div>
-        {{this.$route.params.id}}
-        {{user}}
+
+        <div class="flex flex-col items-stretch w-screen">
+            <p>Jeux</p>
+            <div class="grid grid-cols-2 gap-2">
+                <div v-for="game of games" v-bind:key="game.id"  v-on:click="overlay = !overlay">
+                    <GameCard :game="game">
+                    </GameCard>
+                </div>
+                
+            </div>
+        </div>
+        <v-overlay :value="overlay" v-on:click="overlay = false"></v-overlay>
     </v-main>
 </template>
 
-<script setup lang="ts">
+<script lang="ts">
+import GameCard from '../GameCard.vue';
 
 
-const props = defineProps(['users'])
-const user=props.users[1]
+export default { 
+    components: { GameCard },
+
+    props: ["users", "games"],
+
+    data() {
+        return {
+            usersArray: this.users,
+            id: this.$route.params.id,
+            overlay: false,
+
+        };
+    },
+    computed: {
+        user() {
+            return this.usersArray.find(user => user.id == this.id);
+        }
+    },
+    
+    methods: {
+
+    }
+
+ 
+
+    
+  
+}
+
 </script>
 
   
