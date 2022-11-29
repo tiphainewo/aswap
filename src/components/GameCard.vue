1b7a57e9ebe1886ab58c5ede157e665e78045630
Tiphaine <tiphainewo@gmail.com>
2022-11-29 09:32:59 +0100

    started page with user details

---
+++ src/components/GameCard.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="flex flex-col items-start w-full hover:bg-gray-100 p-2 rounded" v-on:click="$emit('openGame')">
+  
+            <img class="w-full h-30 bg-grey-500 rounded" src="https://images.unsplash.com/photo-1585504198199-20277593b94f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3117&q=80" />
+      
+        <p>{{game.name}}</p>
+        <div class="flex gap-1 items-start">
+            <v-chip x-small>Jeu de plateau</v-chip>
+            <v-chip x-small>2-5 joueurs</v-chip>
+            <v-chip x-small>6+ ans</v-chip>
+        </div>
+    </div>
+
+</template>
+
+
+<script>
+
+export default {
+    props: ['game']
+}
+
+</script>
