29aafc3f6037fef032a6708c76eaec298d517789
Tiphaine <tiphainewo@gmail.com>
2022-11-30 10:18:27 +0100

    push

---
+++ src/components/ChatTextField.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <v-text-field
+            filled
+            rounded
+            dense
+          ></v-text-field>
+    </div>
+  </template>
+
+<script>
+
+
+export default{
+    
+}
+
+</script>
