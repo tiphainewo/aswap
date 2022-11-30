16e9f719ae1032078e782bbe82a08cb4aff16576
Tiphaine <tiphainewo@gmail.com>
2022-11-30 10:57:58 +0100

    chat

--- src/components/pages/ChatPage.vue
+++ src/components/pages/ChatPage.vue
@@ -1,5 +1,5 @@
 <template>
-    <v-main class="flex flex-col items-center gap-10" v-if="user">
+    <v-main class="flex flex-col items-center gap-10 h-[80%]" v-if="user">
         <div class="flex flex-row items-center border-b-2 w-screen gap-3  p-3">
             <v-icon v-on:click="$router.push(`/inbox`);">
                     mdi-arrow-left
@@ -15,10 +15,19 @@
             </v-avatar>
             <div class="font-bold">{{ user.firstName + ' ' + user.lastName }}</div>
         </div>
-        <div class=" p-3 flex flex-col justify-between h-50">
+        <div class=" p-3 flex flex-col justify-between h-full">
         
             <ChatComponent></ChatComponent>
-            <ChatTextField></ChatTextField>
+            <v-text-field
+            append-outer-icon="mdi-send"
+            prepend-icon="mdi-plus-circle"
+            filled
+            rounded
+            dense
+            @click:append-outer="sendMessage"
+            @click:prepend="openBooking"
+            v-model="newMessage"
+          ></v-text-field>
         </div>
         
     </v-main>
@@ -27,23 +36,28 @@
 
 <script lang="ts">
 import ChatComponent from '../ChatComponent.vue';
-import ChatTextField from '../ChatTextField.vue';
-
-
 
 export default { 
     props: ["users"],
 
-    components: {ChatComponent, ChatTextField},
+    components: {ChatComponent},
 
     methods: {
-
+        sendMessage(){
+            console.log(this.newMessage)
+        },
+        openBooking(){
+            this.booking = true;
+            console.log(this.booking)
+        }
     },
 
     data() {
         return {
             usersArray: this.users,
             id: this.$route.params.id,
+            newMessage: "",
+            booking : false,
         };
     },
     computed: {
