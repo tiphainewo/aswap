e7812723c72efe3bb0158c3a583c62f188192e49
Yacine16 <113745630+cicinho16@users.noreply.github.com>
2022-11-30 10:17:49 +0100

    add Catalog

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: [
-    'vuetify'
-  ],
-  pluginOptions: {
-    windicss: {
-      // see https://github.com/windicss/vite-plugin-windicss/blob/main/packages/plugin-utils/src/options.ts
-    },
-  },
+  transpileDependencies: true
 })
